7a9f9e808e1ef2603d3510dffdafeba2033daa82
EncryptSL <Rydlos123@gmail.com>
2025-11-04 07:19:24 +0100

    Try another fix

--- svelte.config.js
+++ svelte.config.js
@@ -36,7 +36,7 @@ const config = {
             // base: process.argv.includes('dev') ? '' : '/my-repo',
         },
 		prerender: {
-			entries: ['*', changelogFiles]
+			entries: ['*', ...changelogFiles]
 		}
 	},
 }
